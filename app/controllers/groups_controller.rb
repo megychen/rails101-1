84a0d7f67cf721a051eaef54e31c9d4916bd525c
megy chen <megy.chen203@gmail.com>
2016-12-08 22:41:37 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,12 +9,14 @@ class GroupsController < ApplicationController
     end
 
     def create
-        @group = Group.new(group_params)
-        if @group.save
-          redirect_to groups_path
-        else
-          render :new
-        end
+      @group = Group.new(group_params)
+      @group.user = current_user
+
+      if @group.save
+        redirect_to groups_path
+      else
+        render :new
+      end
     end
 
     def show
